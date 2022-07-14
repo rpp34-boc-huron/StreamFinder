18c6df6ea6a2daa4212de5705e8eaaf53fc6d462
sase <bs.saase@gmail.com>
2022-07-14 08:48:34 -0400

    init

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      Hello World From React
+    </div>
+  );
+};
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.port || 8080;
+const publicPath = path.join(__dirname, '..', 'client', 'public');
+const indexHtmlPath = path.join(publicPath, 'index.html');
+
+app.use(express.json());
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(indexHtmlPath);
+});
+
+app.listen(port, () => {
+  console.log('Server listening on port: ', port);
+});
