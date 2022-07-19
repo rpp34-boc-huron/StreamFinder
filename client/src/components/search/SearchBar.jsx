3020895da1926e82bc1764a52b9404f1441b7cc8
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-07-18 17:43:41 -0700

    Be able to search with "Enter"

--- client/src/components/search/SearchBar.jsx
+++ client/src/components/search/SearchBar.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import axios from 'axios';
+import Paper from '@mui/material/Paper';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
 
 const SearchBar = () => {
   const [keywords, setKeywords] = useState('');
@@ -15,27 +17,49 @@ const SearchBar = () => {
   }
 
   const handleSearch = () => {
-    event.preventDefault();
-    // console.log('before call api keywords ===>', keywords)
-    axios.get(`/search/movies/${keywords}`)
+    console.log(keywords);
+      axios.get(`/search/movies/${keywords}`)
       .then(result => {
-        // console.log('successfully searched')
-        // console.log('data in searchBar', result.data)
         let path = `/search/movies/${keywords}`;
         navigate(path, { state: { keywords, data: result.data } });
       })
       .catch(err => {
-        console.log('oh no....');
+        console.log('oh no....', err);
       })
   }
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      console.log('search via Enter');
+      handleSearch();
+    }
+    // search when type, change to KeyUp
+    // handleSearch();
+  }
+
+  // return (
+  //   <div className="searchBar">
+  //     <input type='text' placeholder='Search for a movie...' onChange={handleInput} onKeyDown={handleKeyDown}></input>
+  //     <button onClick={handleSearch}>
+  //       <SearchIcon style={searchButtonStyle} />
+  //     </button>
+  //   </div>
+  // );
+
+
   return (
-    <div className="searchBar">
-      <input type='text' placeholder='Search for a movie...' onChange={handleInput}></input>
-      <button onClick={handleSearch}>
-        <SearchIcon style={searchButtonStyle} />
-      </button>
-    </div>
+    <Paper className="search">
+    <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search for a movie..."
+        inputProps={{ 'aria-label': 'search for a movie...' }}
+        onChange={handleInput}
+        onKeyDown={handleKeyDown}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search" onClick={handleSearch}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
