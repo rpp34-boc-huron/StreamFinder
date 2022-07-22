0840bdff3ec229105e93ad395974a1ab2ed296aa
nathan qiu <kechengqiu@gmail.com>
2022-07-22 18:25:25 -0500

    WIP commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import MultiActionAreaCard from './components/hoverCard.jsx';
 
 const App = () => {
   return (
     <div className="app">
       Hello World From React
+      <MultiActionAreaCard></MultiActionAreaCard>
     </div>
   );
 };
