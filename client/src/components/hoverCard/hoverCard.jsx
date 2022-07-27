24f324aa65023ee85e5044339f6c40c5a34c3c5a
Derek Southard <derekmsouthard@gmail.com>
2022-07-26 23:23:08 -0700

    Fixed bugs introduced in movie-hover PR.

--- client/src/components/hoverCard/hoverCard.jsx
+++ client/src/components/hoverCard/hoverCard.jsx
@@ -87,7 +87,7 @@ export default function HoverCard() {
                 />
               </CardActionArea>
             </Card>
-            <AddToFavorites event={{addToFavorite}}/>
+            <AddToFavorites event={{addToFavorites}}/>
             <AddToList />
             </Stack>
           {/* </CardActions> */}
