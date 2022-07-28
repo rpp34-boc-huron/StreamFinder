96383a47b812e9fcd7ba92dd75c1eb7a4619df17
sase <bs.saase@gmail.com>
2022-07-27 21:03:50 -0400

    linked user profile to react router

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingSearchPage from './components/search/LandingSearchPage';
+import UserProfile from './components/profilePage/UserProfile';
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
@@ -14,6 +15,7 @@ root.render(
     <Routes>
       <Route path='/' element={<App />}/>
       <Route path='/search/movies/:keywords' element={<LandingSearchPage />} />
+      <Route path='/profile' element={<UserProfile />} />
     </Routes>
   </BrowserRouter>
 );
