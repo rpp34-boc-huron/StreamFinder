e6d847f25cada4cef824556648f31139d18378a7
sase <bs.saase@gmail.com>
2022-07-28 22:16:08 -0400

    complete desktop-front-end view

--- client/src/components/profile/FriendsList.jsx
+++ client/src/components/profile/FriendsList.jsx
@@ -1,12 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import { TextField, Button } from '@mui/material';
 
 const UserFriends = (props) => {
 
     //
 
     return (
-    <div>
-      User Friends
+    <div className="user-profile-friends">
+      <div className="user-profile-header">Friends</div>
+      <TextField variant="standard" label="Search Friends" size="small" fullWidth/>
+      <Button sx={{width: 'max-content'}}>Add Friend</Button>
+      <div className="user-profile-friends-list"></div>
     </div>
   );
 };
