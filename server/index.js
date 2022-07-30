f1aa96e67c7000d342db53d7ac634f22bbecae3a
sunheal <sunheal@gmail.com>
2022-07-30 16:00:13 -0400

    Separate controllers to moveieSpecific2Controller.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios').default;
 const cors = require('cors');
+const { searchTrailers, searchRecommendations }= require('./controllers/movieSpecific2Controller')
 
 const app = express();
 const port = process.env.port || 8080;
@@ -13,46 +14,8 @@ app.use(express.json());
 app.use(express.static(publicPath));
 app.use(cors());
 
-//------------------movie-specific2------------------//
-const uri = 'https://api.themoviedb.org/3/movie';
-
-app.get('/trailers/:movieId', async (req, res) => {
-  try {
-    const { movieId } = req.params;
-    const apiRes = await axios.get(`${uri}/${movieId}/videos`, {
-      params: {
-        api_key: process.env.API_KEY,
-        language: 'en-US'
-      }
-    });
-    // const trailerKeys = apiRes.data.results.map(trailer => trailer.key);
-    // res.status(200).send(trailerKeys);
-    const trailerKey = apiRes.data.results[0].key;
-    res.status(200).send(trailerKey);
-  } catch (err) {
-    res.status(500).send('get trailer error');
-    console.error('get trailer error', err);
-  }
-});
-
-app.get('/recommendations/:movieId', async (req, res) => {
-  try {
-    const { movieId } = req.params;
-    const apiRes = await axios.get(`${uri}/${movieId}/recommendations`, {
-      params: {
-        api_key: process.env.API_KEY,
-        language: 'en-US'
-      }
-    });
-    const { results } = apiRes.data;
-    res.status(200).send(results);
-  } catch (err) {
-    res.status(500).send('get recommendations error');
-    console.error('get recommendations error', err);
-  }
-})
-
-//------------------movie-specific2------------------//
+app.get('/trailers/:movieId', searchTrailers);
+app.get('/recommendations/:movieId', searchRecommendations);
 
 app.get('/', (req, res) => {
   res.sendFile(indexHtmlPath);
