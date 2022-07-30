b608923efd02040b9802f31fdca8dd9a79eafbc9
sase <bs.saase@gmail.com>
2022-07-30 18:31:07 -0400

    init

---
+++ client/src/components/profile/UserServices.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const UserServices = (props) => {
+  const {user} = props;
+
+  return (
+    <div className="user-profile-services">
+      //
+    </div>
+  );
+};
