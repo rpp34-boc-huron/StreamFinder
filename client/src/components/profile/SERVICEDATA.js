9ab0777b6a435131900cbf767249c9f57bb914f6
sase <bs.saase@gmail.com>
2022-07-30 19:47:33 -0400

    removed url

--- client/src/components/profile/SERVICEDATA.js
+++ client/src/components/profile/SERVICEDATA.js
@@ -1,14 +1,11 @@
 module.exports = {
   Netflix: {
     price: '9.99/m',
-    url: '../../../public/imgs/netflix.png'
   },
   Hulu: {
-    price: '6.99m (ads)',
-    url: '../../../public/imgs/hulu.png'
+    price: '6.99/m',
   },
   HBO: {
     price: '14.99/m',
-    url: '../../../public/imgs/hbo.webp'
   }
 };
