96a04d6ba3a4d57151ce19edd5828ce388e1deaf
sase <bs.saase@gmail.com>
2022-07-30 18:05:47 -0400

    Refactored About me, now uses props mock  user

--- client/src/components/profile/AboutMe.jsx
+++ client/src/components/profile/AboutMe.jsx
@@ -5,13 +5,7 @@ import EditUserInfo from './EditUserInfo.jsx';
 
 const AboutMe = (props) => {
 
-  // const {user} = props;
-  const user = {};
-  const fakeUser = {
-    name: 'Georgeus',
-    pfUrl: '/www.google.com/images/bruh',
-    bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, nemo commodi nisi quos sit minus voluptate odio laboriosam harum deleniti velit aperiam perferendis sequi distinctio, quaerat voluptas aspernatur odit suscipit in tempora ut adipisci? Hic velit, modi inventore labore laboriosam cum tenetur fugiat provident incidunt ipsum doloribus iusto, non ut suscipit itaque rerum iste ad esse. Eos libero non vel quidem illum iste, quod voluptas mollitia consequatur deleniti voluptates, dolore voluptatibus explicabo omnis illo accusantium.'
-  };
+  const {user} = props;
 
   const openFilePicker = () => {
     document.querySelector('#open-file').click();
@@ -22,16 +16,34 @@ const AboutMe = (props) => {
 
     return (
     <div className="user-profile-user-meta-information">
-      <input type="file" id="open-file" style={{display: 'none'}}/>
-      <div className="user-profile-edit-bio"><Button onClick={toggleEditUser} >Edit</Button></div>
       <EditUserInfo toggleEditUser={toggleEditUser} editUser={editUser}/>
+      
+      <div className="user-profile-edit-bio">
+        <input type="file" id="open-file" style={{display: 'none'}}/>
+        <Button onClick={toggleEditUser} >Edit</Button>
+      </div>
+      
       <div className="user-profile-user-meta-image-container">
         <div className="replace-this-image"></div>
-        <div className="replace-profile-image-button" onClick={openFilePicker}><CameraAltIcon sx={{width: '50px', height: '50px', background: 'lightgrey', position: 'relative', left: '25px', borderRadius: '100%', padding: '5px'}}/></div>
+        <div 
+          className="replace-profile-image-button" 
+          onClick={openFilePicker}>
+          <CameraAltIcon 
+            sx={{width: '50px', 
+              height: '50px', 
+              background: 'lightgrey', 
+              position: 'relative', 
+              left: '25px', 
+              borderRadius: '100%', 
+              padding: '5px'
+            }}
+          />
+        </div>
       </div>
+      
       <div className="user-profile-user-meta-about-me-container">
-        <div className="user-profile-username-text">{user.name || fakeUser.name}</div>
-        <div className="user-profile-about-me-text">{user.bio || fakeUser.bio}</div>
+        <div className="user-profile-username-text">{user.username}</div>
+        <div className="user-profile-about-me-text">{user.aboutMe}</div>
       </div>
     </div>
   );
--- client/src/components/profile/UserProfile.jsx
+++ client/src/components/profile/UserProfile.jsx
@@ -3,14 +3,26 @@ import AboutMe from './AboutMe.jsx';
 import AccountSettings from './AccountSettings.jsx';
 import FriendsList from './FriendsList.jsx';
 
+var mockUser = {
+  username: 'Sase',
+  password: 'johnFKenedyTheGoat#imAnImmigrant',
+  profileUrl: '',
+  aboutMe: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab exercitationem suscipit voluptatem sed quaerat dolore dolorem, nesciunt quis fugit odio illum placeat quos? Debitis, laudantium aperiam veritatis facere beatae aliquid officia, veniam distinctio aspernatur mollitia eaque ab laboriosam eveniet quidem molestias autem, ducimus at quas reprehenderit error nemo quibusdam assumenda! Voluptates molestias ab soluta error fugiat. Libero dolore aliquam ratione eveniet rem cum? Omnis inventore eligendi minus amet dolore eaque qui ullam, nobis dolores? Dicta!',
+  ownedServices: ['Netflix'],
+  favorites: [],
+  watchList: [],
+  friends: ['Josh']
+};
+
 const UserProfile = (props) => {
 
-    //
+  const [user, setUser] = useState(mockUser);
+  // User should be passed down...
 
-    return (
+  return (
     <div className="user-profile">
       <div className="user-information-container">
-        <AboutMe />
+        <AboutMe user={user}/>
         <AccountSettings />
       </div>
 
