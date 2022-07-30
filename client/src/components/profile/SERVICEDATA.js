7cc8c9028363c7f2f6578b05174180dbb35a7162
sase <bs.saase@gmail.com>
2022-07-30 19:03:21 -0400

    mock-service-data

---
+++ client/src/components/profile/SERVICEDATA.js
@@ -0,0 +1,14 @@
+module.exports = {
+  Netflix: {
+    price: '9.99/m',
+    url: '../../../public/imgs/netflix.png'
+  },
+  Hulu: {
+    price: '6.99m (ads)',
+    url: '../../../public/imgs/hulu.png'
+  },
+  HBO: {
+    price: '14.99/m',
+    url: '../../../public/imgs/hbo.webp'
+  }
+};
