037f598bd944e04dc346880d5588bed89e61f4fb
nathan qiu <kechengqiu@gmail.com>
2022-08-04 09:58:14 -0500

    finished watchlist, favorites function, fixed display bug

--- server/routes/hoverCard.js
+++ server/routes/hoverCard.js
@@ -2,15 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { addList, addFavorites, getMovieInfo } = require('../controllers/hoverCardController')
-
 //route to grab movie data
 router.get('/details/:id', getMovieInfo)
-//route to post to watch list
-router.post('/favorites', addFavorites)
-
-
 //route to post to favorites
+router.post('/favorites', addFavorites)
+//route to post to watch list
 router.post('/list', addList)
-
 
 module.exports = router;
