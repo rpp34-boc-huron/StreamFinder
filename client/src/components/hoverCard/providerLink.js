0a847e2fe227f30ce1a3ce87e180133f347e333d
nathan qiu <kechengqiu@gmail.com>
2022-08-05 19:16:32 -0500

    added icon links

---
+++ client/src/components/hoverCard/providerLink.js
@@ -0,0 +1,18 @@
+const url = {
+  'Netflix' : 'https://www.netflix.com/',
+  'Hulu': 'https://www.hulu.com/',
+  'Disney Plus': 'https://www.disneyplus.com/',
+  'HBO MAX': 'https://www.hbomax.com/',
+  'Amazon Prime Video': 'https://www.amazon.com/Prime-Video/',
+  'Apple TV Plus': 'https://www.apple.com/apple-tv-plus/',
+  'TNT': 'https://www.tntdrama.com/watchtnt',
+  'TBS':'https://www.tbs.com/watchtbs',
+  'tru TV':'https://www.trutv.com/',
+  'DIRECTV': 'https://www.directv.com/stream/live/',
+  'Paramount+ Amazon Channel': 'https://www.paramountplus.com/',
+  'EPIX Amazon Channel': 'https://www.amazon.com/gp/video/offers?benefitId=epix',
+  'Paramount+ Roku Premium Channel': 'https://channelstore.roku.com/details/32b338eebb16d1de34870b6b1d674911/paramount-plus',
+  'Epix': 'https://www.epix.com/',
+  'Paramount Plus': 'https://www.paramountplus.com/'
+}
+module.exports = url;
